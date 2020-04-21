de3873bbe8726474227b0ccb02e631b03e94cf2b
nicolencinas <nicolencinas@hotmail.com>
2020-04-20 22:33:51 -0300

    Cambiando pagina de reportes

--- arlearn/src/main/java/com/Arlearn/ArlearnApplication.java
+++ arlearn/src/main/java/com/Arlearn/ArlearnApplication.java
@@ -1,4 +1,10 @@
 package com.Arlearn;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +18,8 @@ public class ArlearnApplication {
 	{
 		SpringApplication.run(ArlearnApplication.class, args);
 		Log.info("Holis soy un logger");
+		
+	
 	}
 
 }
