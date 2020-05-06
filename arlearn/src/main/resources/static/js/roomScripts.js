99361669672d86e782d6750e4d6853d37eaa57b0
nicolencinas <nicolencinas@hotmail.com>
2020-05-06 19:37:48 -0300

    Subiendo cambios

--- arlearn/src/main/resources/static/js/roomScripts.js
+++ arlearn/src/main/resources/static/js/roomScripts.js
@@ -35,15 +35,15 @@ function getReservations()
             '<div id="reservations" class="container">'
             +'<img id="key" src="/images/key.png" th:src=@{/images/key.png} th:href=@{/images/key.png}><p id="reservationumber">'+(i+1)+'</p>'
             +'<div class="row first">'
-            +'<div class="col"><p><i class="far fa-calendar-check"></i>Check-in:' +item.beginDate+'</p></div>'
-            +'<div class="col"><p><i class="fas fa-calendar-check"></i>Check-out:'+item.endDate+'</p></div>'
-            +'<div class="col"><p><i class="fas fa-male"></i>Adultos:'+ item.adultsCuantity+'</p><p><i class="fas fa-child"></i>Niños:'+ item.childsCuantity +'</p></div></div>'
+            +'<div class="col"><p><i class="far fa-calendar-check"></i>Check-in: ' +item.beginDate+'</p></div>'
+            +'<div class="col"><p><i class="fas fa-calendar-check"></i>Check-out: '+item.endDate+'</p></div>'
+            +'<div class="col"><p><i class="fas fa-male"></i>Adultos: '+ item.adultsCuantity+'</p><p><i class="fas fa-child"></i>Niños: '+ item.childsCuantity +'</p></div></div>'
             +'<div class="row">'
-    +'<div class="col"><i class="fab fa-hire-a-helper"></i>Tipo de reserva:'+item.reservationType+'</p></div>'
+    +'<div class="col"><i class="fab fa-hire-a-helper"></i>Tipo de reserva: '+item.reservationType+'</p></div>'
     
-    +'<div class="col"><p><i class="fas fa-bed"></i>Tipo de habitacion:'+ item['room'].type+'</p></div>'
+    +'<div class="col"><p><i class="fas fa-bed"></i>Tipo de habitacion: '+ item['room'].type+'</p></div>'
 
-    +'<div class="col categoria"><p><img id="category" src="/images/'+img+'.png" th:src=@{/images/'+img+'.png} th:href=@{/images/'+img+'.png}>'+item['room'].category+'</p></div></div></div>'
+    +'<div class="col categoria"><p><img id="category" src="/images/'+img+'.png" th:src=@{/images/'+img+'.png} th:href=@{/images/'+img+'.png}> Categoria:'+item['room'].category+'</p></div></div></div>'
         );
 
         
